7c89ee842590ae71a5a7f6245a23b6030b5a5080
Akansel Cosgun <akansel.cosgun@monash.edu>
2019-01-30 17:23:47 +1100

    added projector display to table

--- scripts/projectorDisplay.py
+++ scripts/projectorDisplay.py
@@ -3,7 +3,7 @@ import rospy
 import time
 from visualization_msgs.msg import MarkerArray
 from Tkinter import *
-
+import numpy as np
 
 
 initialise = 1
@@ -18,19 +18,43 @@ def callback(data):
     redGoal = data.markers[0].pose.position
     blueGoal = data.markers[1].pose.position
     global initialise
-    displayWidth = 1900
-    displayHeight = 1640
-    goalRad = 30
+    displayWidth = 1920
+    displayHeight = 1080
+    goalRad = 130
 
     print initialise
 
+
+    #print blueGoal
+    # Rotating coordinates
+    temp = redGoal.y
+    redGoal.y = redGoal.x
+    redGoal.x = temp
+
+    temp = blueGoal.y
+    blueGoal.y = blueGoal.x
+    blueGoal.x = temp
+
+    # Translating point to top left of window
+    redGoal.x = redGoal.x + 0.4
+    redGoal.y = redGoal.y + 0.85
+
+    blueGoal.x = blueGoal.x + 0.4
+    blueGoal.y = blueGoal.y + 0.85
     print redGoal
-    print blueGoal
+    # x: -0.17 to -0.85 -->  0 to displayHeight
+    # y: -0.75 to 0.4 -->  0 to displayWidth
+
+    #displayBlue = [(blueGoal.y+1)/2*displayWidth, (abs(blueGoal.x) - 0.17)/0.68*displayHeight]
+    #displayRed = [(redGoal.y+1)/2*displayWidth, (abs(redGoal.x) - 0.17)/0.68*displayHeight]
+    #displayBlue = [(blueGoal.y+0.4)/1.15*displayWidth,(0.68-(abs(blueGoal.x)-0.17))/0.68*displayHeight]
+    #displayRed = [(redGoal.y+0.4)/1.15*displayWidth,(0.68-(abs(redGoal.x)-0.17))/0.68*displayHeight]
+    displayBlue = [blueGoal.x/1.15*displayWidth, blueGoal.y/0.68*displayHeight]
+    displayRed = [redGoal.x/1.15*displayWidth, redGoal.y/0.68*displayHeight]
 
 
-    displayBlue = [(blueGoal.y+1)/2*displayWidth, abs(blueGoal.x)*displayHeight]
-    displayRed = [(redGoal.y+1)/2*displayWidth, abs(redGoal.x)*displayHeight]
-        
+    print displayBlue
+    print displayRed
 
     if(initialise):
     	global top, canvas, red, blue
@@ -59,11 +83,11 @@ def callback(data):
     #pygame.draw.circle(gameDisplay, (255, 0, 0), (int((redGoal.y+1)/2*displayWidth), int(abs(redGoal.x)*displayHeight)), 200)
     #pygame.draw.circle(gameDisplay, (0, 0, 255), (int((blueGoal.y+1)/2*displayWidth), int(abs(blueGoal.x)*displayHeight)), 200)
     #pygame.display.update()
-    
 
 
 
-    
+
+
 def listener():
 
 	rospy.init_node('projectorDisplay', anonymous=True)
@@ -72,11 +96,5 @@ def listener():
 	rospy.spin()
 
 if __name__ == '__main__':
-	
+
 	listener()
-
-
-
-
-
-
